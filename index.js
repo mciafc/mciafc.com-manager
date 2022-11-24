a54079119e4bdef017c94c8ccbb51c578ec9f7b1
carreb <caleb@domyjobfor.me>
2022-11-23 21:13:45 -0500

    add /dash/talent

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
 const cors = require('cors')
-const path = __dirname + '/public/'
+const public = __dirname + '/public/'
+const path = require('path')
 
 
 app.use(cors())
-app.use(express.static(path + 'home/dist'))
+app.use(express.static(public + 'home/dist'))
+app.use(express.static(public + 'dash/talent/dist'))
 
 // homepage
 const homepage = require('./routes/home.js')
@@ -28,5 +30,15 @@ app.use('/sets/:setname', (req, res) => {
 app.use('/equipment', (req, res) => {
     res.redirect(`https://docs.google.com/spreadsheets/d/1TCJ0tCgmkN2dp9DVNebDUcWMQnzhv5vU0iRf5Nc-18E/edit?usp=sharing`)
 })
+
+// DASHBOARD
+
+app.use('/dash/talent', (req, res) => {
+    res.sendFile('index.html', {root: path.resolve('public/dash/talent/dist')})
+})
+
+app.use(["/gigs", "/dash/gigs"], (req, res) => {
+  res.redirect("https://gigs.mciafc.com");
+});
 
 app.listen(1738, () => console.log('Active on PORT 1738'))
